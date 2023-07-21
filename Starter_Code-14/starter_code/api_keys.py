4c441c919ec46bd4e5ed3f43271840da28ca0b47
NataliiaBondarenko <135402681+NatkaBond@users.noreply.github.com>
2023-07-21 10:41:22 -0400

    Update api_keys.py

--- Starter_Code-14/starter_code/api_keys.py
+++ Starter_Code-14/starter_code/api_keys.py
@@ -1,5 +1,6 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "09eb8e9a8fd7b51925d9a6e45f78b4b3"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "021c766f1f97448abd38c91d628ed1e1"
+
